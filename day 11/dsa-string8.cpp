4e74325dca64ac4dd466a0e9e9ef10e127e0dd10
DivyaMulchandani <113537199+DivyaMulchandani@users.noreply.github.com>
2024-07-15 01:21:47 +0530

    day 11

---
+++ day 11/dsa-string8.cpp
@@ -0,0 +1,40 @@
+//longest substring without repeating characters
+
+#include<iostream>
+#include<string>
+#include<set>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    string s = "abcabcbb";
+    set<char> ss;
+    int max_l = 0;
+    int left =0;
+    for(int i=0;i<s.size();i++){
+        while(ss.find(s[i]) != ss.end()){
+            ss.erase(left);
+            left++;
+        }
+        
+        ss.insert(s[i]);
+        max_l = max(max_l,i-left+1);
+
+    }
+    // string s="abcabcbb";
+    // int max_len = 0;
+    // int start = -1;
+    // vector<int> ss(256,-1);
+    // for(int i=0;i<s.size();i++){
+    //     if(ss[s[i]] > start){
+    //         start = ss[s[i]];
+    //     }
+    //     ss[s[i]] = i;
+    //     max_len=max(max_len,i-start);
+    // }
+
+    cout<<max_l;
+
+    return max_l;
+}
